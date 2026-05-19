508b4ad233eb34c58bb8e1c68ae94b09cdf565c1
Tuong Linh Nghiêm <scorpiuslinh@gmail.com>
2026-05-19 05:10:37 +0000

    19/5/26

--- hi3/honkai/mei/mei.js
+++ hi3/honkai/mei/mei.js
@@ -127,7 +127,7 @@ document.addEventListener("DOMContentLoaded", () => {
     playerSwordImg.src = `mei-images/player-sword${stage}.png`;
 
     // enemy sword theo stage
-    enemySwordImg.src = `mei-images/enemy-sword${stage}.png`;
+    enemySwordImg.src = `mei-images/enem-sword${stage}.png`;
 
     // Update stage title
     const stageTitle = document.getElementById("stage-title");
